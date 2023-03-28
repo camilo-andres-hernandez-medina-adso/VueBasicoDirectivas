62dec4bbda6f9f99d71d6e562e6ba453c323c6ed
camilo-andres-hernandez-medina-adso <cahernandez3510@misena.edu.co>
2023-03-27 20:27:31 -0500

    feat: :sparkles: v-bind style
    se utilizo la directiva de v-bind style

--- component/componet.js
+++ component/componet.js
@@ -1,12 +1,14 @@
 export default{
     data(){
         return{
-            className:"box",
-            className2:["box","box2"],
-            className3:{
-                box:true,
-                box2:false,
-                box3:true
+            styleName:"background:red",
+            styleName2:["background: #a1a1a1","color:#fff"],
+            styleName3:{
+                background:"violet",
+                color:"#fff",
+                "font-size":"2rem",
+                "font-family":".5rem",
+                margin:".5rem 0"
             }
         }
     }
