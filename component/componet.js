8b40612a684b081180ba6504c4d69dbaba1f3be9
camilo-andres-hernandez-medina-adso <cahernandez3510@misena.edu.co>
2023-03-27 20:16:57 -0500

    feat: :sparkles: v-for
    se utilizo la directiva de v-for

--- component/componet.js
+++ component/componet.js
@@ -2,11 +2,11 @@ export default{
 
     data(){
         return{
-            mensaje:`<em>mmm</em>`,
-            mensaje2:`<mark>uwu</mark>`,
-            mensaje3:`<u>holamp</u>`,
-            mensaje4:`no cumple `,
-            opcion:null
+            si:[
+                "ryuuzoo",
+                "wuw",
+                "uwu",
+            ]
+        }
         }
     }
-}
