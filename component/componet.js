730f165cd5f88704d5a68c2e3fc102b2b0bdce5e
camilo-andres-hernandez-medina-adso <cahernandez3510@misena.edu.co>
2023-03-27 20:22:01 -0500

    feat: :sparkles: v-bind dinamico
    se utilizo la directiva v-bind dinamico

--- component/componet.js
+++ component/componet.js
@@ -1,9 +1,10 @@
 export default{
-
     data(){
         return{
-    imagen:"https://static-cdn.jtvnw.net/jtv_user_pictures/bec30f59-ecf7-457c-b213-d217cc8e9a2b-profile_image-300x300.png",
-                texto:"uwu"
-            }
+            atributo:"src",
+            imagen:"https://static-cdn.jtvnw.net/jtv_user_pictures/bec30f59-ecf7-457c-b213-d217cc8e9a2b-profile_image-300x300.png",
+            atributo2:"alt",
+            texto:"ryuuzoo"
         }
     }
+}
