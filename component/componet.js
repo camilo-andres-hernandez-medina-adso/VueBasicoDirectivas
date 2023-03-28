4cb243534fbf515944ea5f5b0a640637236ada7e
camilo-andres-hernandez-medina-adso <cahernandez3510@misena.edu.co>
2023-03-27 20:20:08 -0500

    feat: :sparkles: v-bind
    se utilizo la directiva de v-bind

--- component/componet.js
+++ component/componet.js
@@ -2,11 +2,8 @@ export default{
 
     data(){
         return{
-            si:[
-                "ryuuzoo",
-                "wuw",
-                "uwu",
-            ]
-        }
+    imagen:"https://static-cdn.jtvnw.net/jtv_user_pictures/bec30f59-ecf7-457c-b213-d217cc8e9a2b-profile_image-300x300.png",
+                texto:"uwu"
+            }
         }
     }
